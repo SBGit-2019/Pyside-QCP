76f9692905764b431716dc7954ae8439ac524e14
SBGit-2019 <QCP@sbcomputing.de>
2019-11-21 10:18:35 +0100

    Bindings and makefiles.

---
+++ bindings.h
@@ -0,0 +1,25 @@
+/***************************************************************************
+**                                                                        **
+**  QCustomPlot Pyside2 bindings are Python bindings for QCustomPlot/Qt   **
+**                                                                        **
+**                                                                        **
+**  This program is free software: you can redistribute it and/or modify  **
+**  it under the terms of the GNU General Public License as published by  **
+**  the Free Software Foundation, either version 3 of the License, or     **
+**  (at your option) any later version.                                   **
+**                                                                        **
+**  This program is distributed in the hope that it will be useful,       **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+**  GNU General Public License for more details.                          **
+**                                                                        **
+**  You should have received a copy of the GNU General Public License     **
+**  along with this program.  If not, see http://www.gnu.org/licenses/.   **
+**                                                                        **
+****************************************************************************
+**  Website/Contact: https://github.com/SBGit-2019/Pyside-QCP             **
+****************************************************************************/
+
+#define QT_ANNOTATE_ACCESS_SPECIFIER(a) __attribute__((annotate(#a)))
+ 
+#include "qcustomplot.h"
