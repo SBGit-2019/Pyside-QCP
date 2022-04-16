d29e1a58c8ac1a45dab0241e8180c8ee2ffa9bb1
SBGit <58021350+SBGit-2019@users.noreply.github.com>
2022-04-16 18:15:17 +0200

    Init file

---
+++ qcustomplot/__init__.py
@@ -0,0 +1,12 @@
+#from .QCustomPlot import QCP
+#from .QCustomPlot import QCustomPlot
+from .QCustomPlot import *
+
+#__all__ = ['QCustomPlot']
+#__all__ = list("Qt" + body for body in
+#    "Core;Gui;Widgets;PrintSupport;Sql;Network;Test;Concurrent;X11Extras;Xml;XmlPatterns;Help;Multimedia;MultimediaWidgets;OpenGL;OpenGLFunctions;Positioning;Location;Qml;Quick;QuickControls2;QuickWidgets;RemoteObjects;Scxml;Script;ScriptTools;Sensors;SerialPort;TextToSpeech;Charts;Svg;DataVisualization;UiTools;WebChannel;WebEngineCore;WebEngine;WebEngineWidgets;WebSockets;3DCore;3DRender;3DInput;3DLogic;3DAnimation;3DExtras"
+#    .split(";"))
+__version__ = "0.15.2.1"
+__version_info__ = (0, 15, 2.1, "", "")
+
+
